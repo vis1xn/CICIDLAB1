addcb397418585b787d0d20d3bb44df8b6c5b128
vis1xn <g00421432@atu.ie>
2024-10-01 11:55:07 +0100

    Added new endpoint for calculator

--- src/main/java/ie/atu/week3lab1/Calculator.java
+++ src/main/java/ie/atu/week3lab1/Calculator.java
@@ -1,7 +1,6 @@
 package ie.atu.week3lab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
