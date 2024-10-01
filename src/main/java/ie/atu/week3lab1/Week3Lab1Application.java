59044967d77df5796c93108278c3e498dff6c11e
vis1xn <g00421432@atu.ie>
2024-10-01 10:18:39 +0100

    CREATED FIRST LAB

---
+++ src/main/java/ie/atu/week3lab1/Week3Lab1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week3lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3Lab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3Lab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3lab1/Week3Lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3Lab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
